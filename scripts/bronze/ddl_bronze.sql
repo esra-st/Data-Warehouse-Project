274276feafc165125e1bd7a7bd7fe233f69289e1
esra-st <agcilesra06@gmail.com>
2026-04-19 12:59:36 +0300

    Create ddl_bronze.sql

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,76 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+DROP TABLE bronze.crm_cust_info;
+
+
+CREATE TABLE bronze.crm_cust_info(
+cst_id INT,
+cst_key NVARCHAR(50),
+cst_firstname NVARCHAR(50),
+cst_lastname NVARCHAR(50),
+cst_material_status NVARCHAR(50),
+cst_gender NVARCHAR(50),
+cst_create_date DATE
+
+);
+
+CREATE TABLE bronze.crm_prd_info(
+prd_id INT,
+prd_key NVARCHAR(50),
+prd_nm NVARCHAR(50),
+prd_cost NVARCHAR(50),
+prd_line NVARCHAR(50),
+prd_start_dt DATETIME,
+prd_end_dt DATETIME
+);
+
+IF OBJECT_ID('bronze.crm_sales_deatils', 'U') IS NOT NULL
+DROP TABLE bronze.crm_sales_deatils;
+
+
+CREATE TABLE bronze.crm_sales_details(
+sls_ord_num NVARCHAR(50),
+sls_prd_key NVARCHAR(50),
+sls_cus_id INT,
+sls_order_id INT,
+sls_ship_dt INT,
+sls_due_dt INT,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT
+);
+
+CREATE TABLE bronze.erp_loc_a101(
+cid NVARCHAR(50),
+bdate DATE,
+gen NVARCHAR(50)
+);
+DROP TABLE bronze.erp_loc_a101;
+GO
+
+CREATE TABLE bronze.erp_loc_a101 (
+    cid NVARCHAR(50),
+    cntry NVARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_cust_az12(
+cid NVARCHAR(50),
+bdate DATE,
+gen VARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_px_cat_g1v2(
+id NVARCHAR(50),
+cat NVARCHAR(50),
+subcat NVARCHAR(50),
+maintenance NVARCHAR(50)
+);
